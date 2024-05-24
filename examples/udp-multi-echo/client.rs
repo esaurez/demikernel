9a114e00e13027b8fff5de382d5a3cb195ea7d08
Enrique Saurez <esaurez@microsoft.com>
2024-05-24 18:18:09 +0000

    update upd echo client to print latency percentiles

--- examples/udp-multi-echo/client.rs
+++ examples/udp-multi-echo/client.rs
@@ -16,6 +16,7 @@ use demikernel::{
     QDesc,
     QToken,
 };
+use histogram::Histogram;
 use std::{
     collections::HashMap,
     net::SocketAddr,
@@ -60,8 +61,8 @@ pub struct UdpEchoClient {
     npushed: usize,
     /// Number of packets that had to be retried.
     retried: usize,
-    /// Set of connected clients. Each client has an address, a buffer and an index into the buffer. Plus time the first message was received, and the number of messages received.
-    clients: HashMap<QDesc, (SocketAddr, Vec<u8>, usize, Instant, usize)>,
+    /// Set of connected clients. Each client has a buffer and an index into the buffer. Plus time the first message was received, the number of messages received, and the time the last messaged was sent.
+    clients: HashMap<QDesc, (Vec<u8>, usize, Instant, usize, u64)>,
     /// Address of remote peer.
     remote: SocketAddr,
     /// Local ip address
@@ -70,6 +71,10 @@ pub struct UdpEchoClient {
     base_port: u16,
     /// List of pending operations.
     qts: Vec<QToken>,
+    /// Start time.
+    start: Instant,
+    /// Statistics.
+    stats: Histogram,
 }
 
 //======================================================================================================================
@@ -91,18 +96,34 @@ impl UdpEchoClient {
             local_ip,
             base_port,
             qts: Vec::default(),
+            start: Instant::now(),
+            stats: Histogram::new(7, 64)?,
         });
+    }
+
+    fn print_stats(&mut self, time_elapsed: f64) -> Result<()> {
+        let nrequests: f64 = (self.nbytes / self.bufsize) as f64;
+        let rps: f64 = nrequests / time_elapsed;
+        println!(
+            "INFO: {:?} requests, {:2?} rps, p50 {:?} ns, p99 {:?} ns",
+            nrequests,
+            rps,
+            self.stats.percentile(0.50)?.start(),
+            self.stats.percentile(0.99)?.start()
+        );
+        Ok(())
     }
 
     /// Runs the target UDP echo client.
     pub fn run_sequential(
         &mut self,
-        _log_interval: Option<u64>,
+        log_interval: Option<u64>,
         nclients: usize,
         nrequests: Option<usize>,
         niterations: usize,
     ) -> Result<()> {
-        let udp_wait_timeout = Duration::from_millis(1);
+        let udp_wait_timeout = Duration::from_micros(50);
+        let mut last_log: Instant = Instant::now();
 
         for iter in 0..niterations {
             println!("INFO: Starting iteration {}", iter);
@@ -110,35 +131,41 @@ impl UdpEchoClient {
             self.npushed = 0;
             self.nbytes = 0;
             self.retried = 0;
+            self.start = Instant::now();
+            self.stats = Histogram::new(7, 64)?;
+
             // Open all connections.
             for client_id in 0..nclients {
                 let client_local_address = format!("{}:{}", self.local_ip, self.base_port + client_id as u16);
                 let client_socket_address = SocketAddr::from_str(&client_local_address)?;
                 let sockqd: QDesc = self.libos.socket(AF_INET_VALUE, SOCK_DGRAM, 1)?;
-                self.clients.insert(
-                    sockqd.clone(),
-                    (
-                        client_socket_address.clone(),
-                        vec![0; self.bufsize],
-                        0,
-                        Instant::now(),
-                        0,
-                    ),
-                );
+                self.clients
+                    .insert(sockqd.clone(), (vec![0; self.bufsize], 0, Instant::now(), 0, 0));
                 match self.libos.bind(sockqd.clone(), client_socket_address) {
                     Ok(_) => {},
                     Err(e) => {
                         anyhow::bail!("failed to bind socket: {:?}", e);
                     },
                 }
-                self.issue_push(sockqd)?;
+                self.issue_push(sockqd, None)?;
             }
 
             loop {
                 // Stop: enough packets were echoed.
                 if let Some(nrequests) = nrequests {
                     if self.nechoed >= nrequests {
+                        println!("INFO: {} requests completed", self.nechoed);
                         break;
+                    }
+                }
+
+                // Dump statistics.
+                if let Some(log_interval) = log_interval {
+                    if last_log.elapsed() > Duration::from_secs(log_interval) {
+                        let time_elapsed: f64 = (Instant::now() - last_log).as_secs() as f64;
+                        self.print_stats(time_elapsed)?;
+                        last_log = Instant::now();
+                        self.nbytes = 0;
                     }
                 }
 
@@ -149,14 +176,12 @@ impl UdpEchoClient {
                     },
                     Err(e) => {
                         if e.errno == libc::ETIMEDOUT {
-                            // 1 ms timeout expired.
+                            // 50 us timeout expired.
                             self.retried += 1;
-                            // Drop all pending operations.
-                            self.qts.clear();
                             // Push a message to all clients.
                             let clients = self.clients.clone();
-                            for (sockqd, _) in clients.iter() {
-                                self.issue_push(sockqd.clone())?;
+                            for (sockqd, (_, _, _, _, last_time)) in clients.iter() {
+                                self.issue_push(sockqd.clone(), Some(*last_time))?;
                             }
                             continue;
                         } else {
@@ -178,14 +203,18 @@ impl UdpEchoClient {
             }
 
             if let Some(nrequests) = nrequests {
+                let time_elapsed: f64 = (Instant::now() - last_log).as_secs() as f64;
+
                 print!("INFO: Printing for {} requests", nrequests);
+
                 // For each client print the average latency, using the start time in start_time
                 let time_now = Instant::now();
-                for (_qd, (_socket_addr, _buf, _index, start, num_messages)) in &self.clients {
+                for (_qd, (_buf, _index, start, num_messages, _last_message_time)) in &self.clients {
                     let time_elapsed: u64 = (time_now - *start).as_nanos() as u64;
                     let average: u64 = time_elapsed / *num_messages as u64;
                     println!("INFO: Average latency {:?} ns", average);
                 }
+                self.print_stats(time_elapsed)?;
             }
 
             println!(
@@ -197,6 +226,9 @@ impl UdpEchoClient {
             for (qd, _) in self.clients.drain().collect::<Vec<_>>() {
                 self.close_socket(qd)?;
             }
+
+            // Drop all pending operations.
+            self.qts.clear();
         }
 
         Ok(())
@@ -204,7 +236,7 @@ impl UdpEchoClient {
 
     fn close_socket(&mut self, qd: QDesc) -> Result<()> {
         // Send a message with size 1 to signal the server to close the connection
-        let sga: demi_sgarray_t = self.mksga(1, 0)?;
+        let sga: demi_sgarray_t = self.mksga(1, 0, true)?;
         let qt: QToken = match self.libos.pushto(qd, &sga, self.remote) {
             Ok(qt) => qt,
             Err(e) => anyhow::bail!("failed to send close message: {:?}", e),
@@ -224,32 +256,41 @@ impl UdpEchoClient {
     }
 
     // Makes a scatter-gather array.
-    fn mksga(&mut self, size: usize, value: u8) -> Result<demi_sgarray_t> {
-        // Allocate scatter-gather array.
-        let sga: demi_sgarray_t = match self.libos.sgaalloc(size) {
-            Ok(sga) => sga,
-            Err(e) => anyhow::bail!("failed to allocate scatter-gather array: {:?}", e),
-        };
+    fn mksga(&mut self, size: usize, message_time: u64, close_connection: bool) -> Result<demi_sgarray_t> {
+        if !close_connection {
+            debug_assert!(size > std::mem::size_of::<u64>());
+            let sga: demi_sgarray_t = self.libos.sgaalloc(size)?;
+            let ptr: *mut u8 = sga.sga_segs[0].sgaseg_buf as *mut u8;
+            let len: usize = sga.sga_segs[0].sgaseg_len as usize;
+            let slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(ptr, len) };
+            slice[0..8].copy_from_slice(&message_time.to_le_bytes());
+            Ok(sga)
+        } else {
+            // Allocate scatter-gather array.
+            let sga: demi_sgarray_t = match self.libos.sgaalloc(size) {
+                Ok(sga) => sga,
+                Err(e) => anyhow::bail!("failed to allocate scatter-gather array: {:?}", e),
+            };
 
-        // Ensure that scatter-gather array has the requested size.
-        // If error, free scatter-gather array.
-        if sga.sga_segs[0].sgaseg_len as usize != size {
-            Self::freesga(&mut self.libos, sga);
-            let seglen: usize = sga.sga_segs[0].sgaseg_len as usize;
-            anyhow::bail!(
-                "failed to allocate scatter-gather array: expected size={:?} allocated size={:?}",
-                size,
-                seglen
-            );
+            // Ensure that scatter-gather array has the requested size.
+            // If error, free scatter-gather array.
+            if sga.sga_segs[0].sgaseg_len as usize != size {
+                Self::freesga(&mut self.libos, sga);
+                let seglen: usize = sga.sga_segs[0].sgaseg_len as usize;
+                anyhow::bail!(
+                    "failed to allocate scatter-gather array: expected size={:?} allocated size={:?}",
+                    size,
+                    seglen
+                );
+            }
+
+            // Fill in scatter-gather array.
+            let ptr: *mut u8 = sga.sga_segs[0].sgaseg_buf as *mut u8;
+            let len: usize = sga.sga_segs[0].sgaseg_len as usize;
+            let slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(ptr, len) };
+            slice.fill(0);
+            Ok(sga)
         }
-
-        // Fill in scatter-gather array.
-        let ptr: *mut u8 = sga.sga_segs[0].sgaseg_buf as *mut u8;
-        let len: usize = sga.sga_segs[0].sgaseg_len as usize;
-        let slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(ptr, len) };
-        slice.fill(value);
-
-        Ok(sga)
     }
 
     /// Free scatter-gather array and warn on error.
@@ -266,7 +307,7 @@ impl UdpEchoClient {
         let sga: demi_sgarray_t = unsafe { qr.qr_value.sga };
 
         // Retrieve client buffer.
-        let (_, recvbuf, index, _, count): &mut (SocketAddr, Vec<u8>, usize, Instant, usize) = self
+        let (recvbuf, index, _, count, _last_msg_time): &mut (Vec<u8>, usize, Instant, usize, u64) = self
             .clients
             .get_mut(&qd)
             .ok_or(anyhow::anyhow!("unregistered socket"))?;
@@ -279,25 +320,34 @@ impl UdpEchoClient {
 
         // TODO: Sanity check packet.
 
-        // Free scatter-gather-array.
-        self.libos.sgafree(sga)?;
-
         *index += len;
         self.nbytes += len;
 
         // Check if there are more bytes to read from this packet.
         if *index < recvbuf.capacity() {
+            self.libos.sgafree(sga)?;
+
             // There are, thus issue a partial pop.
             let size: usize = recvbuf.capacity() - *index;
             self.issue_pop(qd, Some(size))?;
         }
         // Push another packet.
         else {
+            // Read timestamp from recvbuf.
+            let timestamp: u64 = u64::from_le_bytes([
+                recvbuf[0], recvbuf[1], recvbuf[2], recvbuf[3], recvbuf[4], recvbuf[5], recvbuf[6], recvbuf[7],
+            ]);
+            let now: u64 = Instant::now().duration_since(self.start).as_nanos() as u64;
+            let elapsed: u64 = now - timestamp;
+            self.stats.increment(elapsed)?;
+
+            self.libos.sgafree(sga)?;
+
             // There aren't, so push another packet.
             *count += 1;
             *index = 0;
             self.nechoed += 1;
-            self.issue_push(qd)?;
+            self.issue_push(qd, None)?;
         }
         Ok(())
     }
@@ -347,9 +397,13 @@ impl UdpEchoClient {
     }
 
     /// Issues a push operation
-    fn issue_push(&mut self, qd: QDesc) -> Result<()> {
-        let fill_char: u8 = (self.npushed % (u8::MAX as usize - 1) + 1) as u8;
-        let sga: demi_sgarray_t = self.mksga(self.bufsize, fill_char)?;
+    fn issue_push(&mut self, qd: QDesc, time: Option<u64>) -> Result<()> {
+        let message_time = if let Some(t) = time {
+            t
+        } else {
+            Instant::now().duration_since(self.start).as_nanos() as u64
+        };
+        let sga: demi_sgarray_t = self.mksga(self.bufsize, message_time, false)?;
         let qt: QToken = self.libos.pushto(qd, &sga, self.remote)?;
         self.register_operation(qt);
         Ok(())
